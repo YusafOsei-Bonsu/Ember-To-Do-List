65d73c05e7b9af630a41fd364a97647b09e59f73
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-30 18:39:43 +0100

    Dynamic segment to access a certain todo item

--- app/router.js
+++ app/router.js
@@ -9,6 +9,6 @@ export default class Router extends EmberRouter {
 Router.map(function() {
   this.route('todos', function() {
     this.route('new');
-    this.route('edit');
+    this.route('edit', { path: '/edit/:todo_id' });
   });
 });
