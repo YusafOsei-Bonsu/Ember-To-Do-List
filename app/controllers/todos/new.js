189db5fd1772a5330433f6ad05f6bf5100a87fea
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-10-01 10:33:37 +0100

    The form that adds todo items into firebase through the addTodo()

--- app/controllers/todos/new.js
+++ app/controllers/todos/new.js
@@ -1,4 +1,28 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
 export default class TodosNewController extends Controller {
+    @action
+    addTodo() {
+        let date, title, body;
+        date = this.get('date');
+        title = this.get('title');
+        body = this.get('body');
+
+        // Creating the new todo
+        let newTodo = this.store.createRecord('todo', {
+            title,
+            body,
+            date: new Date(date)
+        });
+
+        // Store in firebase db
+        newTodo.save();
+
+        // Clear form
+        this.setProperties({
+            title: '',
+            body: ''
+        });
+    }
 }
