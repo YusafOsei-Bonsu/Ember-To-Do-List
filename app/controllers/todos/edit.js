bd15baf56e699695795746c83ae5220b01784728
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-10-01 15:45:25 +0100

    Can now change the properties of a to-do item

--- app/controllers/todos/edit.js
+++ app/controllers/todos/edit.js
@@ -1,4 +1,30 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
 export default class TodosEditController extends Controller {
+    @action
+    editTodo(id) {
+        let self, title, body, date;
+        self = this;
+        title = this.get('model.title');
+        body = this.get('model.body');
+        date = this.get('model.date');
+
+        this.store.findRecord('todo', id)
+        .then((todo) => {
+            // Changing properties of to-do item
+            todo.set('title', title);
+            todo.set('body', body);
+            todo.set('date', new Date(date));
+            
+            // Save updates in firebase
+            todo.save();
+
+            // Redirecting to the todos page
+            self.transitionToRoute('todos');
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+    }
 }
